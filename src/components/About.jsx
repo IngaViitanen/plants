06f9521b6798f8f7893ae543097f00ec6397b901
Inga Viitanen <ingaa134@gmail.com>
2024-05-30 00:30:49 +0200

    styled-components

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const About = () => {
   return (
-    <div>About</div>
+    <StyledWrapper>
+        <h1>About</h1>
+    </StyledWrapper>
   )
 }
 
 export default About
+
+const StyledWrapper = styled.main`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+`
--- src/components/YourPlants.jsx
+++ src/components/YourPlants.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const YourPlants = () => {
-  return (
-    <div>YourPlants</div>
-  )
+    return (
+        <StyledWrapper>
+            <h1>Your plants</h1>
+        </StyledWrapper>
+    )
 }
 
 export default YourPlants
+
+const StyledWrapper = styled.main`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+`
