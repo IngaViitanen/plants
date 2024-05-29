22260c38efebd2fac023ccb56a3e742b4408b7ac
Inga Viitanen <ingaa134@gmail.com>
2024-05-29 23:14:47 +0200

    header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <header className='home-header'>
+        <h1>Some inspirational and welcoming text</h1>
+    </header>
   )
 }
 
